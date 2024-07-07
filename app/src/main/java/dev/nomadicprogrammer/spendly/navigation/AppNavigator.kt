28a9766063eb4a5643e4125879270ebef02a03a4
vikash prajapati <prajapativikash0000@gmail.com>
2024-07-07 20:41:12 +0530

    Feature: Screen animation

--- app/src/main/java/dev/nomadicprogrammer/spendly/navigation/AppNavigator.kt
+++ app/src/main/java/dev/nomadicprogrammer/spendly/navigation/AppNavigator.kt
@@ -3,6 +3,10 @@ package dev.nomadicprogrammer.spendly.navigation
 import android.Manifest
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -12,20 +16,35 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import dev.nomadicprogrammer.spendly.base.TransactionStateHolder
 import dev.nomadicprogrammer.spendly.checkAndRequestPermission
 import dev.nomadicprogrammer.spendly.home.presentation.Home
 import dev.nomadicprogrammer.spendly.home.presentation.HomeEvent
 import dev.nomadicprogrammer.spendly.home.presentation.HomeViewModel
 import dev.nomadicprogrammer.spendly.smsparser.transactionsclassifier.model.TransactionType
-import dev.nomadicprogrammer.spendly.transaction.presentation.view.AllTransactions
 import dev.nomadicprogrammer.spendly.transaction.presentation.create.CreateTransaction
+import dev.nomadicprogrammer.spendly.transaction.presentation.view.AllTransactions
 import dev.nomadicprogrammer.spendly.transaction.presentation.view.TransactionDetails
+
+
+fun AnimatedContentTransitionScope<NavBackStackEntry>.enterTransition() : EnterTransition {
+    return slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Left)
+}
+fun AnimatedContentTransitionScope<NavBackStackEntry>.exitTransition() : ExitTransition {
+    return slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Left)
+}
+fun AnimatedContentTransitionScope<NavBackStackEntry>.popEnterTransition() : EnterTransition {
+    return slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Right)
+}
+fun AnimatedContentTransitionScope<NavBackStackEntry>.popExitTransition() : ExitTransition {
+    return slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Right)
+}
+
 
 @Composable
 fun AppNavigator(){
@@ -40,9 +59,23 @@ fun AppNavigator(){
     }
 }
 
+fun NavGraphBuilder.appComposable(route : String, content: @Composable AnimatedContentScope.(NavBackStackEntry) -> Unit){
+    composable(
+        route = route,
+        enterTransition = { enterTransition() },
+        exitTransition = { exitTransition() },
+        popEnterTransition = { popEnterTransition() },
+        popExitTransition = { popExitTransition() }
+    ) { backstackEntry ->
+        content(backstackEntry)
+    }
+}
+
 fun NavGraphBuilder.transactionDetails(navController: NavController, homeViewModel: HomeViewModel){
-    composable(TransactionDetail.route){
-        val transactionStateHolder = homeViewModel.state.collectAsState().value.selectedTransactionalSms?:return@composable
+    appComposable(
+        route = TransactionDetail.route
+    ){
+        val transactionStateHolder = homeViewModel.state.collectAsState().value.selectedTransactionalSms?:return@appComposable
 
         TransactionDetails(
             navController = navController,
@@ -56,13 +89,13 @@ fun NavGraphBuilder.transactionDetails(navController: NavController, homeViewMod
 }
 
 fun NavGraphBuilder.seeAllTransactions(navController: NavController, homeViewModel: HomeViewModel){
-    composable(SeeAllTransaction.route){
+    appComposable(SeeAllTransaction.route){
         AllTransactions(navController, homeViewModel)
     }
 }
 
 fun NavGraphBuilder.newTransaction(navController: NavController){
-    composable(NewTransaction.route){ backstackEntry ->
+    appComposable(NewTransaction.route){ backstackEntry ->
         val transactionToCreateType = backstackEntry.arguments?.getString(NewTransaction.Args.TRANSACTION_TYPE)?.run {
             if (this == "Expense") TransactionType.DEBIT else TransactionType.CREDIT
         }
@@ -71,7 +104,7 @@ fun NavGraphBuilder.newTransaction(navController: NavController){
 }
 
 fun NavGraphBuilder.homeNavGraph(navController: NavController, homeViewModel: HomeViewModel){
-    composable(Home.route){
+    appComposable(Home.route){
         var isPermissionAvailable by remember { mutableStateOf(false) }
 
         val launcher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) {
